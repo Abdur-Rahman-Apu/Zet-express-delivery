57857ead5981b967a16e89a884aec8cc9665e3d9
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-01-22 11:23:58 +0600

    Active link issue is solved

--- js/activePage.js
+++ js/activePage.js
@@ -1,7 +1,9 @@
 const activePage = window.location.pathname;
 
-const navLinks = document.querySelectorAll('header nav .container .collapse ul li a').forEach(link => {
+const navLinks = document.querySelectorAll('header nav .container-lg .collapse ul li a').forEach(link => {
+    console.log(link);
     if (link.href.includes(`${activePage}`)) {
+
         link.classList.add('active-menu')
     }
 })
